39e31ef91bba3c7d45363f3ed7d01093f62227fa
yudinaane <112169806+yudinaane@users.noreply.github.com>
2023-04-14 12:33:58 +0500

    Add files via upload

---
+++ first_test.py
@@ -0,0 +1,22 @@
+import pytest
+from app.calculator import Calculator
+
+
+class TestCalc:
+    def setup(self):
+        self.calc = Calculator
+
+    def test_multiply_calculate_correctly(self):
+        assert self.calc.multiply(self, 2, 2) == 4
+
+    def test_multiply_calculation_failed(self):
+        assert self.calc.multiply(self, 2, 2) == 5
+
+    def test_division_calculate_correctly(self):
+        assert self.calc.division(self, 2, 2) == 1
+
+    def test_subtraction_calculate_correctly(self):
+        assert self.calc.subtraction(self, 3, 2) == 1
+
+    def test_adding_calculate_correctly(self):
+        assert self.calc.adding(self, 2, 2) == 4
